0df2e0162c07a0b9ad6b9c043580da96ee3ae0ef
mackemo <mackenzielmoore14@gmail.com>
2024-03-28 12:50:25 -0400

    added comment messages in all of the code

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -4,7 +4,7 @@ const backButton = document.querySelector('#back-button');
 const postsList = document.querySelector('#posts-list');
 
 let mode = 'light';
-
+//Event Listener for toggle button (light/dark mode)//
 toggleButton.addEventListener('click', function() {
     if (mode === 'light') {
         mode = 'dark';
@@ -14,14 +14,14 @@ toggleButton.addEventListener('click', function() {
         container.setAttribute('class', 'light');
     }
 });
-
+//Back button goes back to main page//
 backButton.addEventListener('click', function() {
     location.href = "index.html"; 
 })
-
+//Turning local storage into objects to display on page//
 const posts = JSON.parse(localStorage.getItem('blogPosts')) || [];
 postsList.innerHTML = '';
-
+//For loop displaying each blog post on page//
 for (let i = 0; i < posts.length; i++) {
     const post = posts[i];
     const postLi = document.createElement('li');
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,19 @@ const title = document.querySelector('#blog-title');
 const content = document.querySelector('#blog-content');
 const submitButton = document.querySelector('#submit');
 
+//Event Listener for submit button//
 submitButton.addEventListener('click', function(e) {
     e.preventDefault();
-
+    //Blog post content//
     const blogPost = {
         user: user.value.trim(),
         title: title.value.trim(),
         content: content.value.trim(),
     };
-
+    //Alerting if form isnt complete//
     if (blogPost.user === "" || blogPost.title === "" || blogPost.content === "") {
         alert("Please complete form");
+    //Storing input in local storage and switching to blog page//
     } else {
         const oldPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
         oldPosts.push(blogPost);
